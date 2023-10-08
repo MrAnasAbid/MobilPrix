983e3f15fa1e89de75efa9d6f2d3360c43d6b538
MrAnasAbid <m.abid.anas@gmail.com>
2023-10-08 17:08:42 +0200

    Enhanced the treeview of the project

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,6 @@ import pickle
 
 columns = ['battery_power', 'clock_speed', 'fc', 'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height', 'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time']
 
-with open("./src/LogReg.p", 'rb') as pickled:
+with open("./models/LogReg.p", 'rb') as pickled:
     data = pickle.load(pickled)
     log_reg_model = data["Best_LogReg"]
